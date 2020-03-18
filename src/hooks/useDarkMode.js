56c6a3e89fc44b93081699e6c530ff47a33ff3b9
kkdev211 <kkdev211@users.noreply.github.com>
2020-03-17 23:49:41 -0400

    dark-mode hook

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,7 +1,19 @@
 import {useLocalStorage} from "./useLocalStorage";
+import { useEffect } from "react";
 
 export function useDarkMode(key, initialValues) {
     const [someValue, setSomeValue] = useLocalStorage("darkMode");
     
+    useEffect (()=>{
+        
+    if(localStorage.getItem("darkMode")) {
+        document.body.classList.add("darkMode");
+    }   
+    else {
+        document.body.classList.remove("darkMode");
+        } 
+    }, [someValue]);  
+    return [someValue, setSomeValue];
+     
 }
 
